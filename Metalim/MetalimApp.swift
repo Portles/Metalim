e31f8ae8edfd33fd078a5be5c2e33846d3daef88
Portles <nizamet005@gmail.com>
2023-11-21 21:43:31 +0300

    Initial Commit

---
+++ Metalim/MetalimApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalimApp.swift
+//  Metalim
+//
+//  Created by Nizamet Özkan on 21.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MetalimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
